1e92bd4bf22d58ef6bd712c43cf8385398241699
Mike Li <1726582664@qq.com>
2023-03-26 19:46:53 +0800

    搭建发布者框架

---
+++ build/plumbing_pub_sub/CMakeFiles/demo01_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo01_pub.dir/src/demo01_pub.cpp.o"
+  "/home/ysl/ysl_ws/devel/lib/plumbing_pub_sub/demo01_pub.pdb"
+  "/home/ysl/ysl_ws/devel/lib/plumbing_pub_sub/demo01_pub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo01_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
