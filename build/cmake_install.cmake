d7a1437227ba49b22452b7f7e2def5f6bf74013a
ubuntu_ipc <1726582664@qq.com>
2023-03-27 22:20:59 +0800

    [202303272221]P68服务端实现 1.添加了demo01_server.cpp函数 2.修改了对应的CMakeLists.txt 3.修改了对应的package.xml 4.添加了AddInts.srv文件

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/opt/ysl_ws/build/gtest/cmake_install.cmake")
   include("/home/opt/ysl_ws/build/plumbing_pub_sub/cmake_install.cmake")
+  include("/home/opt/ysl_ws/build/plumbing_server_client/cmake_install.cmake")
 
 endif()
 
---
+++ build/plumbing_server_client/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/opt/ysl_ws/src/plumbing_server_client
+# Build directory: /home/opt/ysl_ws/build/plumbing_server_client
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
