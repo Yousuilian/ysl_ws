d59d8a39281df1647560cc076da96a09174e2adf
Mike Li <1726582664@qq.com>
2023-03-30 03:21:36 +0800

    [202303300321][P124-P135]常用API
    1.Modified demo01_apis_pub.cpp、CMakeLists.txt
    2.Added demo02_apis_sub.cpp、demo03_apis_time.cpp、demo04_apis_log.cpp
    3.Modified README.md

--- src/plumbing_apis/src/demo01_apis_pub.cpp
+++ src/plumbing_apis/src/demo01_apis_pub.cpp
@@ -27,21 +27,49 @@ int main(int argc, char *argv[])
     //使用AnonymousName可以解决节点多次启动问题，否则启动第二个节点时第一个节点会shutdown
     ros::init(argc,argv,"Pub",ros::init_options::AnonymousName);
     ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<std_msgs::String>("fang",10);
+    /*
+        作用：创建发布者对象
+
+        模板：被发布的消息类型
+
+        参数：
+            1.话题名称
+            2.队列长度
+            3.latch（可选） 如果设置为true，会保存发布方的最后一条消息，并且
+              新的订阅对象连接到发布方时，发布方会将这条消息发送给订阅者 
+        使用：
+            latch 设置为 true 的作用？
+            以静态地图发布为例，
+            方案1：可以使用固定频率发送地图数据，但是效率低
+            方案2：可以将地图发布对象的latch 设置为true，并且发布方只发送一次数据，
+            每当订阅者连接时，将地图数据发送给订阅者（只发送一次），这样提高了数据发送的效率
+            (advertise的第三个参数latch的使用，也就是说发布方发完10个数据后再去订阅订阅不到，
+            但是将latch设置为true的时候，发布方发完10个数据后再订阅，因为最后一个数据被保存了
+            所以可以订阅到最后一条数据)
+    */
+    ros::Publisher pub = nh.advertise<std_msgs::String>("fang",10,true);
     std_msgs::String msg;
-    ros::Rate rate(5);
+    ros::Rate rate(2);
     int count = 0;
     ros::Duration(3).sleep();
     while (ros::ok)
     {
+        // if (count >= 10)
+        // {
+        //     ros::shutdown();
+        // }
         count++;
         std::stringstream ss;
         ss << "hello --->"<< count;
         msg.data = ss.str();
-        pub.publish(msg);
-        ROS_INFO("发布的数据是：%s",ss.str().c_str());
+        if (count <= 20)
+        {
+            pub.publish(msg);
+            ROS_INFO("发布的数据是：%s",ss.str().c_str());
+        }
         rate.sleep();
-        ros::spinOnce();
+        ros::spinOnce();//每执行一次就跳转到回调函数一次然后回来继续往下执行
+        ROS_INFO("一轮回调执行完毕......");
     }
     return 0;
 }
